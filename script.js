c69e050f044ce4d9646c11084c94705b1b090181
unknown <szakalyzs@gmail.com>
2020-11-01 22:08:13 +0100

    Kész

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ function kepez() {
             szam++;
         }
         document.write('</div>');
+    }   
+}
+
+function validalas() {
+    let jo = true;
+    var email = document.forms["urlap"]["email"].value;
+    let i = 0;
+    let hossz = email.length;
+    while(i < hossz && email[i] != "@") {
+        i++
     }
-    
+    if(i >= hossz) {
+        alert("Érvénytelen e-mail cím!");
+        jo = false;
+    }
+    var uzenet = document.forms["urlap"]["uzenet"].value;
+    hossz = uzenet.length;
+    if(hossz < 20) {
+        alert("Túl rövid üzenet!");
+        jo = false;
+    }
+    if(jo) alert("Üzenet elküldve!");
 }
